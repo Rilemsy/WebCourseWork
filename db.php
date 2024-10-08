164e3b954586409cd2d5a30928f5389729845ccd
Rilemsy <96448428+Rilemsy@users.noreply.github.com>
2024-10-08 13:42:36 +0300

    Add registration
    Registration form adds username and password into db.

--- db.php
+++ db.php
@@ -1,13 +1,16 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "3719";
 $dbname = "chat_application";
-
+echo "Allo";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) 
 {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
+else
+{
+    echo "Connected";
+}
--- login.php
+++ login.php
@@ -6,7 +6,7 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
-$stmt->bind_param("s", $username);
+$stmt->bind_param("s", $username);                                          // s - string
 $stmt->execute();
 $result = $stmt->get_result();
 $user = $result->fetch_assoc();
@@ -16,7 +16,7 @@ if ($user && password_verify($password, $user['password']))
     $_SESSION['user_id'] = $user['id'];
     $_SESSION['role'] = $user['role'];
     $_SESSION['is_blocked'] = $user['is_blocked'];
-    header("Location: index.html");
+    header("Location: chat.html");
 } 
 else 
 {
